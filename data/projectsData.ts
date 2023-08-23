5f35897a9dc90794f5d8140f6ab258d9cf8718af
Benny Rahmat <akunbeben@gmail.com>
2023-08-23 16:49:31 +0800

    write first article

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,47 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Futuca (Banjarmasin, Indonesia)',
+    description: `Futuca: The New You's product aids seminar attendees by assessing abilities, interests & talents via a self-assessment quiz.
+    Built using TailwindCSS, Alpine, Livewire, Laravel. It was very challanging to build dynamic forms of quiz with many different types of quiz.`,
+    imgSrc: '/static/images/futuca/landing.png',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Veldeva (Banjarmasin, Indonesia)',
+    description: `Online learning platform khusus untuk meningkatkan skill digital, materi diisi oleh Mentor dari Praktisi Industri.
+    Built using Laravel in the Backend, VueJS (NuxtJS) in the Frontend.`,
+    imgSrc: '/static/images/veldeva/landing.png',
+    href: 'https://veldeva.id/',
+  },
+  {
+    title: 'WLS Booking System (Banjarmasin, Indonesia)',
+    description: `Booking platform for a creative space where collaboration, entrepreneur, and education meets. Built using Laravel.`,
+    imgSrc: '/static/images/wls/landing.png',
+    href: 'https://booking.wetlandsquare.id',
+  },
+  {
+    title: 'Eligibilty Checking (Lucknow, India)',
+    description: `I created a versatile form with TailwindCSS, AlpineJS, Livewire, Laravel.
+    It handles various submissions like Personal and Business Loan eligibility checks, each with unique backend calculation formulas.
+    To ensure data consistency, I use DTO as a data abstraction layer.`,
+    imgSrc: '/static/images/wincapital/landing.png',
+  },
+  {
+    title: 'Bridge (Lufkin, TX)',
+    description: `Very niche CRM system for a photography and videography company.
+    I've created customer checkout feature for their gallery, and helping team to integrate the system to the Stripe payment and subscription.
+    Built using Laravel.`,
+    imgSrc: '/static/images/bridge/home.png',
+  },
+  {
+    title: 'PEDEVE ERP (Jakarta, Indonesia)',
+    description: `Collaborating with team members to rewrite and upgrade old (2005) ERP system from ASPX to Laravel.`,
+    imgSrc: '/static/images/pedeve/home.png',
+  },
+  {
+    title: 'Open Journal System (Jakarta, Indonesia)',
+    description: `Custom Open Journal System for P3MB Indonesia. P3MB is an Government Quality Assurance Service in Indonesia.`,
+    imgSrc: '/static/images/bpsmb/home.png',
+    href: 'https://p3mb.or.id'
   },
 ]
 
