7ffab0a3b447727d6891d4eeeeb3bd7a8e75c42d
Benny Rahmat <akunbeben@gmail.com>
2023-08-24 03:51:09 +0800

    update

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,26 +6,26 @@ const projectsData = [
     href: 'https://akunbeben.vercel.app/blog/finally-building-my-portfolio-a-web-developer-journey',
   },
   {
-    title: 'Futuca (Banjarmasin, Indonesia)',
+    title: 'Futuca (Banjarmasin, ID)',
     description: `Futuca: The New You's product aids seminar attendees by assessing abilities, interests & talents via a self-assessment quiz.
     Built using TailwindCSS, Alpine, Livewire, Laravel. It was very challanging to build dynamic forms of quiz with many different types of quiz.`,
     imgSrc: '/static/images/futuca/landing.png',
   },
   {
-    title: 'Veldeva (Banjarmasin, Indonesia)',
+    title: 'Veldeva (Banjarmasin, ID)',
     description: `Online learning platform khusus untuk meningkatkan skill digital, materi diisi oleh Mentor dari Praktisi Industri.
     Built using Laravel in the Backend, VueJS (NuxtJS) in the Frontend.`,
     imgSrc: '/static/images/veldeva/landing.png',
     href: 'https://veldeva.id/',
   },
   {
-    title: 'WLS Booking System (Banjarmasin, Indonesia)',
+    title: 'WLS Booking System (Banjarmasin, ID)',
     description: `Booking platform for a creative space where collaboration, entrepreneur, and education meets. Built using Laravel.`,
     imgSrc: '/static/images/wls/landing.png',
     href: 'https://booking.wetlandsquare.id',
   },
   {
-    title: 'Eligibilty Checking (Lucknow, India)',
+    title: 'Eligibilty Checking (Lucknow, IN)',
     description: `I created a versatile form with TailwindCSS, AlpineJS, Livewire, Laravel.
     It handles various submissions like Personal and Business Loan eligibility checks, each with unique backend calculation formulas.
     To ensure data consistency, I use DTO as a data abstraction layer.`,
@@ -39,12 +39,12 @@ const projectsData = [
     imgSrc: '/static/images/bridge/home.png',
   },
   {
-    title: 'PEDEVE ERP (Jakarta, Indonesia)',
+    title: 'PEDEVE ERP (Jakarta, ID)',
     description: `Collaborating with team members to rewrite and upgrade old (2005) ERP system from ASPX to Laravel.`,
     imgSrc: '/static/images/pedeve/home.png',
   },
   {
-    title: 'Open Journal System (Jakarta, Indonesia)',
+    title: 'Open Journal System (Jakarta, ID)',
     description: `Custom Open Journal System for P3MB Indonesia. P3MB is an Government Quality Assurance Service in Indonesia.`,
     imgSrc: '/static/images/bpsmb/home.png',
     href: 'https://p3mb.or.id'
