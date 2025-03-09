7827eaa0ef3f261a214d5ff33d1d6c346afe852f
Benny Rahmat <akunbeben@gmail.com>
2025-03-10 06:17:53 +0700

    fix build

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,9 +1,21 @@
 import Image from './Image'
 import Link from './Link'
 
-const Card = ({ title, description, imgSrc, href }) => (
+const Card = ({
+  title,
+  description,
+  imgSrc,
+  href,
+}: {
+  title: string
+  description: string
+  imgSrc: string | undefined
+  href: string | undefined
+}) => (
   <div className="md max-w-[544px] p-4 md:w-1/2">
-    <div className={`${imgSrc && 'h-full'} overflow-hidden rounded-md border border-gray-300 border-opacity-60 dark:border-gray-700`}>
+    <div
+      className={`${imgSrc && 'h-full'} border-opacity-60 overflow-hidden rounded-md border border-gray-300 dark:border-gray-700`}
+    >
       {imgSrc &&
         (href ? (
           <Link href={href} aria-label={`Link to ${title}`}>
